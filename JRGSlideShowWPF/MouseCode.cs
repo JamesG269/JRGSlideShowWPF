4588149cefbbda5ff0c492840f3ab77bda7eee5f
James Gentile <jamesraymondgentile@gmail.com>
2018-09-17 20:58:26 -0500

    Removed unused code.

--- JRGSlideShowWPF/MouseCode.cs
+++ JRGSlideShowWPF/MouseCode.cs
@@ -16,13 +16,7 @@ namespace JRGSlideShowWPF
         Point mouseStartPoint = new Point(0, 0);
 
         int MouseWheenCount = 0;
-        int MouseOneIntCount = 0;
-
-        private void Window_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-            //IgnoreNext = true;
-            //maximize();
-        }
+        int MouseOneIntCount = 0;        
 
         private void mouseWheel(object sender, MouseWheelEventArgs e)
         {
@@ -39,58 +33,6 @@ namespace JRGSlideShowWPF
             {
                 displayPrevImage();
             }
-        }
-        
-        Boolean Pressed = false;
-        private void Window_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
-        {/*
-            if (e.LeftButton != MouseButtonState.Pressed)
-            {                
-                return;
-            }
-            
-            if (WindowState == WindowState.Maximized && IgnoreNext == true)
-            {
-                //IgnoreNext = false;
-                //return;
-            }
-            //Vector mouseDiff = mouseStartPoint - e.GetPosition(null);
-            //if (Math.Abs(mouseDiff.X) > SystemParameters.MinimumHorizontalDragDistance || Math.Abs(mouseDiff.Y) > SystemParameters.MinimumVerticalDragDistance)
-           */ 
-
-                                                        
-        }
-        
-        private void Window_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            //mouseStartPoint = e.GetPosition(null);
-            if (e.ClickCount == 2)
-            {
-                maximize();
-            }            
-        }
-        Stack myStack = new Stack();
-        private void WindowToCenter(int height, int width)
-        {            
-            var oldLeft = Left;
-            var oldTop = Top;
-
-            myStack.Push(width);
-            myStack.Push(height);            
-
-            System.Drawing.Point point = Control.MousePosition;
-            this.Left = point.X - (width / 2);
-            this.Top = point.Y - (height / 2);
-
-            
-
-           // MessageBox.Show("OldLeft = " + oldLeft + " OldTop = " + oldTop + " Point.X = " + point.X + " point.Y = " + point.Y + " Left = " + Left + " Top = " + Top);
-        }
-
-        private void Window_DragEnter(object sender, System.Windows.DragEventArgs e)
-        {
-            MessageBox.Show("Drag");
-
         }
         
         private void OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -123,7 +65,7 @@ namespace JRGSlideShowWPF
                 var point = PointToScreen(e.MouseDevice.GetPosition(this));
 
                 Left = point.X - (RestoreBounds.Width * 0.5);
-                Top = point.Y;
+                Top = point.Y - (RestoreBounds.Height * 0.5);
 
                 WindowState = WindowState.Normal;
                 DragMove();
